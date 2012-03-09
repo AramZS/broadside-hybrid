711ca0340a3da0cfba5eaa622655c20775f72dd0
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-03-09 16:23:21 -0500

    Got foreach working to list categories. Needs styling.

---
+++ sidebar-left.php
@@ -0,0 +1,38 @@
+<?php
+global $options;
+foreach ($options as $value) {
+    if (get_settings( $value['id'] ) === FALSE) { $$value['id'] = $value['std']; } else { $$value['id'] = get_settings( $value['id'] ); }
+}
+
+$exploded_left_cats = explode(",", $bh_left_cats);
+
+?><div id="left-cats"><?php
+foreach ($exploded_left_cats as $value) {
+
+	?>
+		<div class="left-cat-box">
+			<div class="left-cat-title">
+				<?php echo get_the_category_by_ID($value); ?>
+			</div>
+	<?php
+	
+			$leftcatquery = new WP_Query( array( 'cat' => $value, 'offset' => 2, 'showposts' => 5) )
+				?><ul><?php
+					while ($leftcatquery->have_posts() ) : $leftcatquery->the_post();
+						?><li>
+							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" alt="<?php the_title_attribute(); ?>"><?php
+								the_title();
+							?></a>
+						</li><?php
+					endwhile;
+				?></ul>
+			
+		</div><?php
+					
+	
+	
+
+} //end foreach
+?></div><?php
+
+?>
